d1b0e743c35540b2e6a8f7c93764164d9b135541
李想 <lixiang@zentech-inc.com>
2017-09-30 16:41:41 +0800

    Initial commit

---
+++ foreman-job-core/src/main/java/org/foreman/App.java
@@ -0,0 +1,13 @@
+package org.foreman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
