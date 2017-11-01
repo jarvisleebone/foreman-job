2d21e53abd9a6ae235ca7cc9c1bdaa9c6a822072
李想 <lixiang@zentech-inc.com>
2017-11-01 16:03:28 +0800

    console project init commit

---
+++ foreman-job-console/foreman-job-console-back/src/main/java/org/foreman/FJConsoleBackApplication.java
@@ -0,0 +1,12 @@
+package org.foreman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FJConsoleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FJConsoleBackApplication.class, args);
+    }
+}
